40f54ca39a49ac8775e59f741491ebbf7b36e376
azeynalli90 <ali.zeynalli@web.de>
2021-12-03 21:44:59 +0100

    adding fe

---
+++ fe-personalspace/apps/personalspace/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fe-personalspace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'personalspace';
+}
